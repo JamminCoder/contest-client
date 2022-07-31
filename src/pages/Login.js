738b6e87a31dadd9a93c94dfa992ace2db1594d6
JamminCoder <jammincoderguy96@gmail.com>
2022-07-31 18:55:03 -0400

    Login form now sets `jwt` cookie if log in is successful
    Added `axios` to deps

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import Form from '../components/Form';
+const axios = require('axios').default;
 
 
 export default function Login(props) {
@@ -7,17 +8,37 @@ export default function Login(props) {
             <h1 className="text-3xl">Login</h1>
             <div className="my-5">
                 <div className="mb-4">
-                    <label for="username">Username</label><br/>
+                    <label htmlFor="username">Username</label><br/>
                     <input type="text" name="username" id="username"/>
                 </div>
 
                 <div className="mb-4">
-                    <label for="password">Password</label><br/>
+                    <label htmlFor="password">Password</label><br/>
                     <input type="password" name="password" id="password"/>
                 </div>
             </div>
             
-            <button className="border border-gray-500 px-2 py-1 rounded">Login</button>
+            <button onClick={ (e) => {
+                e.preventDefault();
+
+                const password = document.querySelector("#password").value;
+                const username = document.querySelector("#username").value;
+                console.log("Password: " + password);
+                console.log("Username: " + username);
+
+                axios.post("http://localhost:8000/login", 
+                    {username: username, password: password}
+                ).then(res => {
+                    if (res.data.jwt) {
+                        console.log("JWT is " + res.data.jwt);
+                        document.cookie = `jwt=${ res.data.jwt }; SameSite=None; Secure`;
+                    } else {
+                        console.log("Incorrect username or password!");
+                    }
+                    
+                })
+
+            } } className="border border-gray-500 px-2 py-1 rounded">Login</button>
         </Form>
     );
 }
